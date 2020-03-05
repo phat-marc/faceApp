965d45b10e27624fdd93880aebefbd9a69539662
It's meee! Phat Marc <phfatm@gmail.com>
2020-03-05 11:43:06 +0200

    initial commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div>
+			<div className='white f3'>
+				{'Place holder name rank'}
+			</div>
+			<div className='white f1'>
+				{'Rank #..'}
+			</div>
+		</div>
+	);
+}
+
+export default Rank;
