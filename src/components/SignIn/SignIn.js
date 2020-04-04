475df7242de808491c6b839c3f72787f92d1e1df
It's meee! Phat Marc <phfatm@gmail.com>
2020-04-04 13:38:12 +0200

    update

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -65,7 +65,7 @@ class SignIn extends React.Component {
 				    </fieldset>
 				    <div className="">
 				      <input 
-				      	onClick={this.onSubmitSignin}
+				      	onClick={this.onSubmitSignIn}
 					      className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
 					      type="submit" 
 					      value="Sign In" 
