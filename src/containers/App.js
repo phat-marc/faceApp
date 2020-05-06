83d3daf6dbd6aba5eac5a2a7bcfd944e87378972
It's meee! Phat Marc <phfatm@gmail.com>
2020-05-06 11:15:54 +0200

    code review security

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
 import Navigation from '../components/Navigation/Navigation';
 import Signin from '../components/Signin/Signin';
@@ -9,12 +8,7 @@ import Logo from '../components/Logo/Logo';
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
 import Rank from '../components/Rank/Rank';
 import '../containers/App.css';
-
-// This will come out  
-const app = new Clarifai.App({
- apiKey: 'b31981ec82f64cf4a5ea01633511218b'
-});
-
+   
 const particlesOptions = {
   particles: { 
     number: {
@@ -81,19 +75,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    // gona swap these around later
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
-      // fetch('http://localhost:3000/', {
-      //   method: 'post',
-      //   headers: {'Content-Type': 'application/json'},
-      //   body: JSON.stringify({
-      //     input: this.state.input
-      //   })    
-      // })
-      // .then(response => response.json())
+      fetch('http://localhost:5000/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })    
+      })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('http://localhost:5000/image', {
